956ee3b3278de95744f327209e2959bdf7615598
KelvinVasconcelos <kelvinvasconcelos2001@gmail.com>
2021-10-15 16:56:56 -0300

    :bug: corrigido padding da mensagem inicial.

--- src/components/logo/styles.tsx
+++ src/components/logo/styles.tsx
@@ -2,13 +2,11 @@ import styled from 'styled-components/native';
 
 export const Container = styled.ScrollView`
   flex: 1;
-  background-color: red;
   margin-bottom: 0;
 `;
 
 export const BannerContainer = styled.View`
   height: 40%;
-  background-color: green;
   justify-content: center;
   align-items: center;
   padding: 8px 0;
@@ -22,13 +20,13 @@ export const Message = styled.Text`
   color: #023F6C;
   font-weight: bold;
   font-size: 18px;
-  padding: 16px;
+  padding: 38px;
   padding-bottom: 0;
 `;
 
 export const SubMessage = styled.Text`
   color: #023F6C;
   font-size: 16px;
-  padding: 16px;
+  padding: 38px;
   padding-top: 0;
 `;
