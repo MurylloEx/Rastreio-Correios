e50f65651cf090a79317a143cb6e1ae788e73898
Muryllo <muryllopimenta@gmail.com>
2021-10-15 16:49:29 -0300

    :sparkles: feature: some changes in styles.

--- src/components/logo/styles.tsx
+++ src/components/logo/styles.tsx
@@ -1,21 +1,20 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-  width: 100%;
-  margin: 48px auto;
+export const Container = styled.ScrollView`
+  flex: 1;
+  background-color: red;
   margin-bottom: 0;
-  max-height: 40%;
-  max-width: 70%;
 `;
 
 export const BannerContainer = styled.View`
+  height: 40%;
+  background-color: green;
   justify-content: center;
   align-items: center;
   padding: 8px 0;
 `;
 
 export const Banner = styled.Image`
-  height: 70%;
   max-height: 180px;
 `;
 
@@ -23,10 +22,13 @@ export const Message = styled.Text`
   color: #023F6C;
   font-weight: bold;
   font-size: 18px;
+  padding: 16px;
+  padding-bottom: 0;
 `;
 
 export const SubMessage = styled.Text`
   color: #023F6C;
   font-size: 16px;
-  padding: 8px 0;
+  padding: 16px;
+  padding-top: 0;
 `;
