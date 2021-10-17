7af8c28b69a01e66efd44ec30e865a293f09ac53
Muryllo <muryllopimenta@gmail.com>
2021-10-17 15:00:26 -0300

    :sparkles: feature: Beginning packageItem.

---
+++ src/components/packageItem/index.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent, Fragment } from 'react';
+
+export type PackageItemProps = {
+  trackCode: string,
+  descriptiion: string
+}
+ 
+export const PackageItem: FunctionComponent<PackageItemProps> = () => {
+  return (
+    <Fragment>
+
+    </Fragment>
+  );
+}
